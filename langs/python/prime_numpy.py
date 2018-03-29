7e931caa89bb45c11c10203b1e76cc4ec4f40dda
taskie <t@skie.jp>
2018-03-30 01:09:34 +0900

    python: use size

--- langs/python/prime_numpy.py
+++ langs/python/prime_numpy.py
@@ -5,17 +5,17 @@ import numpy as np
 def sieve(size):
     if n <= 1:
         return np.zeros(size, bool)
-    sieveArray = np.ones(size, bool)
-    sieveArray[:2] = False
+    sieve_array = np.ones(size, bool)
+    sieve_array[:2] = False
     root = int(math.sqrt(size)) + 1
     for i in range(2, root):
-        if not sieveArray[i]:
+        if not sieve_array[i]:
             continue
-        sieveArray[i * i::i] = False
-    return sieveArray
+        sieve_array[i * i::i] = False
+    return sieve_array
 
-def countPrime(n):
+def count_prime(n):
     return np.count_nonzero(sieve(n + 1))
 
 n = int(sys.argv[1]) if (len(sys.argv) >= 2) else 10000000
-print(countPrime(n))
+print(count_prime(n))
