6423fd9517d8937548b275123a2a319dc015b271
taskie <t@skie.jp>
2018-03-30 01:15:58 +0900

    versions

--- utils/versions.rb
+++ utils/versions.rb
@@ -4,6 +4,7 @@ puts "# versions of compilers and processors"
 puts
 
 commands_for_lang = {
+  'Bash' => ['bash --version | head -1'],
   'C' => ['gcc --version | head -1', 'clang --version | head -1'],
   'C++' => ['g++ --version | head -1', 'clang++ --version | head -1'],
   'C\# #' => ['mono --version | head -1', 'mcs --version | head -1'],
@@ -12,10 +13,12 @@ commands_for_lang = {
   'D' => ['dmd --version | head -1', 'ldc --version | head -2'],
   'Fortran' => ['gfortran --version | head -1'],
   'Go' => ['go version | head -1'],
+  'Groovy' => ['groovy -v 2>&1 | tail -1'],
   'Haskell' => ['ghc --version | head -1'],
   'Java' => ['java -version 2>&1 | tail -1', 'javac -version 2>&1 | tail -1'],
   'JavaScript' => ['node --version | head -1'],
   'Julia' => ['julia --version | head -1'],
+  'Kotlin' => ['kotlin -version'],
   'Lua' => ['lua -v | head -1', 'luajit -v | head -1'],
   'Nim' => ['nim --version 2>&1 | head -1'],
   'OCaml' => ['ocaml --version | head -1'],
